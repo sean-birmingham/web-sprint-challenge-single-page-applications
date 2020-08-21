184bf7a487194840d9e0e5312bb4fb721c63b084
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-21 10:31:52 -0400

    set up pizza order form

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -4,6 +4,46 @@ const PizzaForm = () => {
   return (
     <div>
       <h2>Build Your Own Pizza</h2>
+      <form>
+        <label>
+          Name:&nbsp;
+          <input type='text' name='name' />
+        </label>
+        <label>
+          Choice of Size:&nbsp;
+          <select name='size'>
+            <option value='' disabled hidden>
+              Select a Size
+            </option>
+            <option value='Small'>Small</option>
+            <option value='Medium'>Medium</option>
+            <option value='Large'>Large</option>
+            <option value='Extra-Large'>Extra-Large</option>
+          </select>
+        </label>
+        <h3>Add Toppings</h3>
+        <label>
+          <input type='checkbox' name='pepperoni' />
+          Pepperoni
+        </label>
+        <label>
+          <input type='checkbox' name='sausage' />
+          Sausage
+        </label>
+        <label>
+          <input type='checkbox' name='onions' />
+          Onions
+        </label>
+        <label>
+          <input type='checkbox' name='extra-cheese' />
+          Extra Cheese
+        </label>
+        <label>
+          Special Instructions:
+          <input type='text' name='instructions' placeholder='Anything else?' />
+        </label>
+        <button type='submit'>Add to Order</button>
+      </form>
     </div>
   );
 };
